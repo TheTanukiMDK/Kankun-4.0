9e25dd0f8f58e18616568033ca27fab299b4e757
SnowyMiau <riquicraft04@gmail.com>
2025-02-18 16:57:05 -0500

    Nueva estructura

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home"
+import Login from "../pages/Login"
 
 export const rutas = createBrowserRouter([
     {
         path: "/",
         element: <Home></Home>
-    }
+    },
+    {
+        path: "/Login",
+        element: <Login></Login>
+    },
+
 ])
