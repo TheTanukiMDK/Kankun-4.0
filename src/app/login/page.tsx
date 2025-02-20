3ebb059215af425bc133b48c08ee9658a614ecc9
navia <navivani.contact@gmail.com>
2025-02-20 16:37:45 -0500

    Nuevo formato mejorado, añadidas dependencias y plantillas

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const Root = () => {
+  return (
+    <div>
+      <p>Editen aqui para cambiar la pagina</p>
+    </div>
+  );
+};
+
+export default Root;
---
+++ src/components/kankunlogo.tsx
@@ -0,0 +1,12 @@
+import { TreePalm } from "lucide-react";
+
+const Logo = () => {
+  return (
+    <div className="flex flex-row gap-4 h-32">
+      <TreePalm />
+      <p className="text-2xl font-bold">Kankun 4.0</p>
+    </div>
+  );
+};
+
+export default Logo;
